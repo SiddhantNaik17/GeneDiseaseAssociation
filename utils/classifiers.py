137792b3796af1bc90d0a7627abfcf4f6ba1ea0e
SiddhantNaik17 <siddhantnaik17@gmail.com>
2022-04-15 13:35:48 +0530

    Rewrite classifiers util

--- utils/classifiers.py
+++ utils/classifiers.py
@@ -7,93 +7,37 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from tabulate import tabulate
 
-
-def classify_SVM(_X_train, _X_test, _y_train, _y_test):
-    clf = svm.SVC(kernel='linear')
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("SVM Accuracy:", acc)
-    return acc
-
-
-def classify_DTC(_X_train, _X_test, _y_train, _y_test):
-    clf = DecisionTreeClassifier(criterion='entropy', random_state=0)
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("DT Accuracy:", acc)
-    return acc
+classifiers = {
+    'SVM': svm.SVC(kernel='linear'),
+    'DT': DecisionTreeClassifier(criterion='entropy', random_state=0),
+    'ET': ExtraTreesClassifier(random_state=0),
+    'LDA': LinearDiscriminantAnalysis(),
+    'KNN': KNeighborsClassifier(n_neighbors=7),
+    'GNB': GaussianNB(),
+    'MLP': MLPClassifier(alpha=1e-5, hidden_layer_sizes=(15,), random_state=1),
+    'RF': RandomForestClassifier(max_depth=2, random_state=0)
+}
 
 
-def classify_EDTC(_X_train, _X_test, _y_train, _y_test):
-    clf = ExtraTreesClassifier(random_state=0)
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("ET Accuracy:", acc)
-    return acc
+def _classify(x_train, x_test, y_train, y_test, classifier):
+    classifier.fit(x_train, y_train)
+    y_pred = classifier.predict(x_test)
+    return metrics.accuracy_score(y_test, y_pred)
 
 
-def classify_LDA(_X_train, _X_test, _y_train, _y_test):
-    """Linear Discriminant Analysis"""
-    clf = LinearDiscriminantAnalysis()
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("LDA Accuracy:", acc)
-    return acc
-
-
-def classify_KNN(_X_train, _X_test, _y_train, _y_test):
-    """k-nearest neighbors"""
-    clf = KNeighborsClassifier(n_neighbors=7)
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("KNN Accuracy:", acc)
-    return acc
-
-
-def classify_GNB(_X_train, _X_test, _y_train, _y_test):
-    """Gaussian Naive Bayes"""
-    gnb = GaussianNB()
-    y_pred = gnb.fit(_X_train, _y_train).predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("GNB Accuracy:", acc)
-    return acc
-
-
-def classify_MLP(_X_train, _X_test, _y_train, _y_test):
-    """Multi-layer Perceptron classifier"""
-    clf = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("MLP Accuracy:", acc)
-    return acc
-
-
-def classify_RFC(_X_train, _X_test, _y_train, _y_test):
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf.fit(_X_train, _y_train)
-    y_pred = clf.predict(_X_test)
-    acc = metrics.accuracy_score(_y_test, y_pred)
-    # print("RFC Accuracy:", acc)
-    return acc
-
-
-def classify(X_train, X_test, y_train, y_test, results=None, fs='None'):
+def classify(x_train, x_test, y_train, y_test, results=None, fs='None'):
     result = [
-        ['SVM', classify_SVM(X_train, X_test, y_train, y_test)],
-        ['Decision Tree', classify_DTC(X_train, X_test, y_train, y_test)],
-        ['Extra Trees', classify_EDTC(X_train, X_test, y_train, y_test)],
-        ['Linear Discriminant Analysis', classify_LDA(X_train, X_test, y_train, y_test)],
-        ['kNN', classify_KNN(X_train, X_test, y_train, y_test)],
-        ['Gaussian Naive Bayes', classify_GNB(X_train, X_test, y_train, y_test)],
-        ['Multi-layer Perceptron', classify_MLP(X_train, X_test, y_train, y_test)],
-        ['Random Forest', classify_RFC(X_train, X_test, y_train, y_test)],
+        ['SVM', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['SVM'])],
+        ['Decision Tree', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['DT'])],
+        ['Extra Trees', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['ET'])],
+        ['Linear Discriminant Analysis', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['LDA'])],
+        ['kNN', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['KNN'])],
+        ['Gaussian Naive Bayes', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['GNB'])],
+        ['Multi-layer Perceptron', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['MLP'])],
+        ['Random Forest', _classify(x_train, x_test, y_train, y_test, classifier=classifiers['RF'])],
     ]
+
     if results is not None:
         results.append([fs] + [x[1] for x in result])
+
     print(tabulate(result, tablefmt='fancy_grid'))
