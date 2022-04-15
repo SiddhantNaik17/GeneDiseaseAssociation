80f41d54d6bd79841c5a271a03922b80789908ef
SiddhantNaik17 <siddhantnaik17@gmail.com>
2022-04-15 17:44:25 +0530

    Initial Commit

---
+++ classifiers.py
@@ -0,0 +1,99 @@
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm, metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from tabulate import tabulate
+
+
+def classify_SVM(_X_train, _X_test, _y_train, _y_test):
+    clf = svm.SVC(kernel='linear')
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("SVM Accuracy:", acc)
+    return acc
+
+
+def classify_DTC(_X_train, _X_test, _y_train, _y_test):
+    clf= DecisionTreeClassifier(criterion='entropy', random_state=0)
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("DT Accuracy:", acc)
+    return acc
+
+
+def classify_EDTC(_X_train, _X_test, _y_train, _y_test):
+    clf = ExtraTreesClassifier(random_state=0)
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("ET Accuracy:", acc)
+    return acc
+
+
+def classify_LDA(_X_train, _X_test, _y_train, _y_test):
+    """Linear Discriminant Analysis"""
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("LDA Accuracy:", acc)
+    return acc
+
+
+def classify_KNN(_X_train, _X_test, _y_train, _y_test):
+    """k-nearest neighbors"""
+    clf = KNeighborsClassifier(n_neighbors=7)
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("KNN Accuracy:", acc)
+    return acc
+
+
+def classify_GNB(_X_train, _X_test, _y_train, _y_test):
+    """Gaussian Naive Bayes"""
+    gnb = GaussianNB()
+    y_pred = gnb.fit(_X_train, _y_train).predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("GNB Accuracy:", acc)
+    return acc
+
+
+def classify_MLP(_X_train, _X_test, _y_train, _y_test):
+    """Multi-layer Perceptron classifier"""
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(15,), random_state=1)
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("MLP Accuracy:", acc)
+    return acc
+
+
+def classify_RFC(_X_train, _X_test, _y_train, _y_test):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(_X_train, _y_train)
+    y_pred = clf.predict(_X_test)
+    acc = metrics.accuracy_score(_y_test, y_pred)
+    # print("RFC Accuracy:", acc)
+    return acc
+
+
+def classify(X_train, X_test, y_train, y_test, results=None, fs='None'):
+    result = [
+        ['SVM', classify_SVM(X_train, X_test, y_train, y_test)],
+        ['Decision Tree', classify_DTC(X_train, X_test, y_train, y_test)],
+        ['Extra Trees', classify_EDTC(X_train, X_test, y_train, y_test)],
+        ['Linear Discriminant Analysis', classify_LDA(X_train, X_test, y_train, y_test)],
+        ['kNN', classify_KNN(X_train, X_test, y_train, y_test)],
+        ['Gaussian Naive Bayes', classify_GNB(X_train, X_test, y_train, y_test)],
+        ['Multi-layer Perceptron', classify_MLP(X_train, X_test, y_train, y_test)],
+        ['Random Forest', classify_RFC(X_train, X_test, y_train, y_test)],
+    ]
+    if results is not None:
+        results.append([fs] + [x[1] for x in result])
+    print(tabulate(result, tablefmt='fancy_grid'))
---
+++ utils.py
@@ -0,0 +1,234 @@
+import math
+import random
+import re
+
+from sklearn.svm import SVC
+
+
+def getSamples(filePath):
+    """ Read data from data file.
+
+    Parameters
+    ----------
+    filePath : A string, the path of the data file
+
+    Returns
+    -------
+    A 2D List object, each row represents all gene expression values of a sample
+    """
+    file = open(filePath)
+    firstLine = file.readline()
+    # Get data size
+    numberOfSamples = len(re.split(r'\t+', firstLine)) - 1
+    numberOfGenes = open(filePath).read().count('\n') - 1
+    # Init
+    samples = [[None for x in range(numberOfGenes)] for y in range(numberOfSamples)]
+
+    # Read data
+    i = 0
+    count = 0
+    for line in file.readlines():
+        geneExpression = line.split()
+        # Ignore blank lines
+        if len(geneExpression) == 0:
+            continue
+        # Each row of data:
+        # The first value is the name of the gene (geneName = geneExpression[0]),
+        # The second value starts with the expression value of the gene
+        currentGeneExpression = geneExpression[1:]
+        # Read the data in the file into an array
+        j = 0
+        for value in currentGeneExpression:
+            samples[j][i] = 'None' if value == 'null' else float(value)
+            j += 1
+        i += 1
+        count += 1
+    file.close()
+    return samples
+
+
+def zScoreNormalization(samples):
+    """Z-Score Normalization.
+
+    Z-Score is used to normalize all gene expression data.
+
+    Parameters
+    ----------
+    samples : 1 2D List, containing the gene expression values of all samples
+    """
+    averageValues = []
+    stdVarianceValues = []
+
+    if len(samples) == 0:
+        raise Exception('No samples.')
+    else:
+        # Calculate the mean and standard deviation of each gene
+        # (Only calculate the first row)
+        numberOfGenes = len(samples[0])
+        for index in range(0, numberOfGenes):
+            average, stdVariance = getAverageAndVarianceAmongSamples(samples, index)
+            averageValues.append(average)
+            stdVarianceValues.append(stdVariance)
+
+    # Z-Score Normalization
+    for i, sample in enumerate(samples):
+        for j, value in enumerate(sample):
+            average = averageValues[j]
+            stdVariance = stdVarianceValues[j]
+            samples[i][j] = (value - average) / stdVariance
+
+    return samples
+
+
+def getAverageAndVarianceAmongSamples(samples, index):
+    """Obtain the mean and standard deviation of the expression of a gene in the entire sample.
+
+    And use the calculated mean to fill in missing values.
+
+    Parameters
+    ----------
+    samples :  1 2D List, containing the gene expression values of all samples
+    index : 1 int value, indicating the index value of the column to be calculated
+
+    Returns
+    -------
+    2 double values, respectively representing the mean and standard deviation of a certain gene value
+    """
+    # The sum of all gene expression values
+    sum = 0
+    # Number of non-missing values
+    numberOfExistingValue = 0
+    # Sample mean and standard deviation
+    average = 0
+    stdVariance = 0
+
+    # Calculate the sample mean
+    for sample in samples:
+        thisValue = sample[index]
+
+        if thisValue != 'None':
+            numberOfExistingValue += 1
+            sum += thisValue
+    average = sum / numberOfExistingValue
+
+    # Calculate sample variance
+    for dummyIndex, sample in enumerate(samples):
+        thisValue = sample[index]
+
+        if thisValue != 'None':
+            stdVariance += (thisValue - average) * (thisValue - average)
+        else:
+            sample[index] = average
+    stdVariance = math.sqrt(stdVariance / (numberOfExistingValue * 1.0))
+
+    return average, stdVariance
+
+
+def getTraningAndTestingSamples(samples, sampleIndexes, numberOfTraningSamples, numberOfTestingSamples):
+    """Generate training data and test data from samples.
+
+    Parameters
+    ----------
+    samples: 1 2D List, containing the gene expression values of all samples (dimension reduction)
+    sampleIndexes: 1 1DList, indicating the sample index values of can be sampled
+    numberOfTraningSamples: 1 int value, sampling as the number of training samples
+    numberOfTestingSamples: 1 int value, sampled as the number of test samples
+
+    Returns
+    -------
+    2 1D Lists, which store (dimension-reduced) training samples and (dimension-reduced) test samples respectively
+    """
+    trainingSampleIndexes = random.sample(sampleIndexes, numberOfTraningSamples)
+    testingSampleIndexes = random.sample(list(set(sampleIndexes) - set(trainingSampleIndexes)), numberOfTestingSamples)
+
+    trainingSamples = []
+    testingSamples = []
+
+    for index in trainingSampleIndexes:
+        trainingSamples.append(samples[index])
+    for index in testingSampleIndexes:
+        testingSamples.append(samples[index])
+
+    return trainingSamples, testingSamples
+
+
+def getClassificationErrorSamples(trainingSamples, trainingSampleLabels, testingSamples, testingSampleLabels):
+    """Evaluate the classification accuracy of the reduced data.
+
+    Use the reduced data to train an SVM classifier and test the classification accuracy.
+
+    Parameters
+    ----------
+    trainingSamples: A 2D List containing the gene expression values of training samples
+    trainingSampleLabels: A 1D List, Class Labels containing training samples
+    testingSamples: A 2D List containing the gene expression values of test samples
+    testingSampleLabels: A 1D List containing Class Labels of test samples
+
+    Returns
+    -------
+    5 int type data:
+    -The first parameter indicates the number of misclassified samples on the training set
+    -The second parameter represents TP (True Positive)
+    -The third parameter represents FP (False Positive, Predicted Positive)
+    -The fourth parameter represents FN (False Negative, Predicted Negative)
+    -The fifth parameter means TN (False Negative)
+    """
+    # Create SVM
+    clf = SVC()
+    clf.fit(trainingSamples, trainingSampleLabels)
+    predictedTrainingSampleLabels = clf.predict(trainingSamples)
+    predictedTestingSampleLabels = clf.predict(testingSamples)
+
+    # Compare Classification Result
+    numberOfTrainingSamples = len(trainingSampleLabels)
+    numberOfErrorTrainingSamples = 0
+    numberOfTestingSamples = len(testingSampleLabels)
+    tp = 0
+    fp = 0
+    fn = 0
+    tn = 0
+
+    # Training Error Statistics
+    for i in range(0, numberOfTrainingSamples):
+        if trainingSampleLabels[i] != predictedTrainingSampleLabels[i]:
+            numberOfErrorTrainingSamples = numberOfErrorTrainingSamples + 1
+
+    # Testing Error Statistics
+    # 0 stands for Normal, 1 stands for Tumor
+    for i in range(0, numberOfTestingSamples):
+        if testingSampleLabels[i] == 0 and predictedTestingSampleLabels[i] == 0:
+            # TP
+            tp += 1
+        elif testingSampleLabels[i] == 1 and predictedTestingSampleLabels[i] == 0:
+            # FP
+            fp += 1
+        elif testingSampleLabels[i] == 0 and predictedTestingSampleLabels[i] == 1:
+            # FN
+            fn += 1
+        elif testingSampleLabels[i] == 1 and predictedTestingSampleLabels[i] == 1:
+            # TN
+            tn += 1
+
+    return numberOfErrorTrainingSamples, tp, fp, fn, tn
+
+
+def load_dataset(normalFilePath, tumorFilePath):
+    # Sample of Normal People
+    normalSamples = getSamples(normalFilePath)
+    numberOfNormalSamples = len(normalSamples)
+
+    # Samples of Tumor People
+    tumorSamples = getSamples(tumorFilePath)
+    numberOfTumorSamples = len(tumorSamples)
+
+    # All Samples
+    samples = normalSamples + tumorSamples
+    numberOfSamples = numberOfNormalSamples + numberOfTumorSamples
+
+    # Gene Indexes in List: samples
+    normalSampleIndexes = range(0, numberOfNormalSamples)
+    tumorSampleIndexes = range(numberOfNormalSamples, numberOfSamples)
+
+    # numberOfGenes = len(samples[0]) if len(samples) != 0 else 0
+
+    return samples, normalSampleIndexes, tumorSampleIndexes
